a393b01102c9749f2542358f18c4e1887ab06e2f
caryxiao <289872225@qq.com>
2021-06-07 01:26:24 +0800

    [feat] 优化命令行参数逻辑

--- frpc/src/args.rs
+++ frpc/src/args.rs
@@ -22,11 +22,14 @@ pub fn init() -> Option<(Command, Matches)> {
     opts.optopt("c", "", "frpc config file", "c_file");
     opts.optflag("h", "help", "Print this help menu");
 
-    if args.len() == 1 || args[1] == "-h" {
+    if args.len() == 1 {
+        panic!("Missing parameter.");
+    }
+
+    if args[1] == "-h" {
         help(&program, &opts);
         return None;
     }
-
 
     let command = match args[1].as_ref() {
         "start" => Command::Start,
@@ -34,7 +37,7 @@ pub fn init() -> Option<(Command, Matches)> {
         "reload" => Command::Reload,
         "stop" => Command::Stop,
         "status" => Command::Status,
-        _ => panic!("Please input command"),
+        _ => panic!("Please input command!"),
     };
 
     if args.len() > 2 {
